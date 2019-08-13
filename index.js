c7c53b9090bac544763adcae843807fe3ca81ee6
Jared Bydeley <jared@bydeley.ca>
2019-08-13 11:27:01 -0400

    Include google-protobuf dependency and make using package easier

---
+++ index.js
@@ -0,0 +1,7 @@
+const { UserUpdatedMessage } = require('./dist/user_updated_pb');
+const { AccountUpdatedMessage } = require('./dist/account_updated_pb');
+
+module.exports = {
+  AccountUpdatedMessage,
+  UserUpdatedMessage,
+}
