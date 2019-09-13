628fa519bf6283704a3f17f7f4caeda6ada83775
Gord Tanner <gord@roadmunk.com>
2019-09-13 11:14:27 -0400

    export missing events

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const { UserUpdatedMessage } = require('./dist/user_updated_pb');
 const { AccountUpdatedMessage } = require('./dist/account_updated_pb');
+const { SignupAccountRequest } = require('./dist/signup_account_pb');
+const { AccountSignupCompleted } = require('./dist/account_signup_pb');
 
 module.exports = {
   AccountUpdatedMessage,
   UserUpdatedMessage,
-}
+	SignupAccountRequest,
+	AccountSignupCompleted,
+};
